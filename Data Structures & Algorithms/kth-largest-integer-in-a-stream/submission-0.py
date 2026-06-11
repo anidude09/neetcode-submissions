29e508970b42dcf075a14c1ff123d2a843956bc7
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-06-11 13:20:18 -0500

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,17 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k 
+        self.arr = nums      
+
+    def add(self, val: int) -> int:
+
+        self.arr.append(val)
+        self.arr.sort(reverse=True)
+        return self.arr[self.k - 1]
+
+            
+
+        
+
+        
