8f681c6609b45e8801f2e520b322eacc0324c645
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-06-13 14:15:48 -0500

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        mem = {}
+
+        def dfs(i, bought): 
+
+            if i >= len(prices) : return 0 
+
+            if (i, bought) in mem: 
+                return mem[(i, bought)]
+
+            res = dfs(i+1, bought) #skipping, not doing anything (buy or sell)
+
+            if bought: 
+                res = max(res, prices[i] + dfs(i + 1, False))
+            else:
+                res = max(res, -prices[i] + dfs(i + 1, True))
+            mem[(i, bought)] = res
+            return res
+            
+        return dfs(0, False)
+
+
+
+
+        
+        
