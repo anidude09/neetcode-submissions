82315441d93789d05d31360db74df1ef2f72b813
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-06-13 13:29:07 -0500

    Add: house-robber - submission-9

---
+++ Data Structures & Algorithms/house-robber/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        sums = [-1] * len(nums)
+
+        def dfs(i): 
+            if i >= len(nums): return 0 
+
+            if sums[i] != -1 : return sums[i]
+            
+            sums[i] =  max(dfs(i+1) , dfs(i+2) + nums[i])
+
+            return sums[i]
+        
+        return dfs(0)
+
+
+
+
+
+
+       
+
+
+
+        
+        
+        
+            
+
+
+        
