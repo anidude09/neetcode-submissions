b38dd79becd378d293c2afe1caa011fc8750695c
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-06-14 15:59:16 -0500

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+
+        #kadane's algorithm
+
+        cur =res =  nums[0]
+
+        for i in range(1, len(nums)):
+
+            cur = max(nums[i], cur + nums[i])
+            res = max(cur, res)
+        
+        return res
+
+    
+
+
+        
+
+        
+
+        
