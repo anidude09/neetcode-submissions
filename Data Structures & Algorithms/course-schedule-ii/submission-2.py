2a13b9e4efe28f3583fa3100dd3d96a4ef4aa1d7
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-08-03 23:52:24 -0500

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+
+        adj = {i : [] for i in range(numCourses)}
+
+        for course, preq in prerequisites:
+            adj[preq].append(course)
+
+        
+        safe = set()
+        visiting = set()
+        stack = []
+
+        def dfs(node):
+            if node in visiting:
+                return False
+            if node in safe:
+                return True
+
+            
+            visiting.add(node)
+
+            for n in adj[node]:
+                if not dfs(n):
+                    return False
+                
+            visiting.remove(node)
+            safe.add(node)
+            stack.append(node)
+
+            return True
+        
+        for n in range(numCourses):
+            if not dfs(n):
+                return []
+
+        result = []
+        while stack:
+            cur = stack.pop()
+
+            result.append(cur)
+            
+        return result
+
+
+
+        
+
+        
+
+
