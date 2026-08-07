9b6889412848113f334c9ebcb7cb550717020ffc
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-08-07 15:47:58 -0500

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,45 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+        
+        k = len(s1)
+
+        need = [0]* 26
+        for char in s1:
+            need[ord(char) - ord("a")] += 1
+        
+
+        window = [0] * 26
+        for right in range(len(s2)):
+            window[ord(s2[right]) - ord("a")] += 1
+
+            if right >= k:
+                window[ord(s2[right - k]) - ord("a")] -= 1
+
+            if right >= k - 1 and window == need:
+                return True
+        return False
+            
+            
+
+
+
+       
+
+
+
+
+                
+                
+
+
+        
+        
+         
+
+
+
+         
+        
