992a770714566347737152d38dfa0ce300563e7c
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-08-30 21:59:59 -0700

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+
+        memo = {}
+
+        def solve(index, balance):
+            if balance == 0:
+                return 1
+            if index == len(coins) or balance < 0:
+                return 0 
+            
+            if (index, balance) in memo:
+                return memo[(index, balance)]
+            
+            use_coin = solve(index, balance - coins[index])
+            skip_coin = solve(index + 1, balance)
+
+            memo[(index, balance)] = use_coin + skip_coin
+
+            return memo[(index, balance)]
+
+        return solve(0, amount)
+
+        
+
+
+
+
+
+
+        
+
+        
+        
