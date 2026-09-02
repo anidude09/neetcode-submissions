83f3116e6b849c422eba4b6ba22393170704d0c2
Aniruddh <66564474+anidude09@users.noreply.github.com>
2026-09-02 00:11:42 -0700

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -0,0 +1,24 @@
+
+
+from bisect import bisect_left
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        tail = []
+
+        for num in nums:
+            index = bisect_left(tail, num)
+
+            if index == len(tail):
+                tail.append(num)
+            
+            else:
+                tail[index] = num
+            
+        return len(tail)
+
+
+
+
+        
